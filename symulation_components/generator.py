eefad588f0c038922c853a302ee6150618879033
KarolK99 <konkolisz@gmail.com>
2022-06-14 01:00:15 +0200

    Implement LoadDistribution class and Change PassangerGenerator to wait for external call

--- symulation_components/generator.py
+++ symulation_components/generator.py
@@ -1,10 +1,8 @@
 from abc import ABC, abstractmethod
 
-import time
 import pykka
 import random
 import networkx as nx
-from threading import Thread
 from typing import List
 
 from pykka import ActorProxy
@@ -14,71 +12,39 @@ from symulation_components.passenger import Passenger
 
 
 class LoadDistribution:
-    def __init__(self):
-        pass
+    load: List[int]
 
-    @staticmethod
-    def get_dist(vehicle, route):
-        return 2 if vehicle.id == 1 else 3
+    def __init__(self, load: List[int]) -> None:
+        self.load = load
 
-    def get_number_of_passengers(self, iteration):
-        return 2
+    def get_number_of_passengers(self, time: float):
+        index = int(time) % len(self.load)
+        return self.load[index]
 
 
 class AbstractPassengerGenerator(ABC, pykka.ThreadingActor):
     map: Map
-    hour: int
-    frequency: int  # hours per one iteration
-
-    _thread_loop: Thread
 
     @abstractmethod
-    def on_start(self) -> None:
-        pass
-
-    @abstractmethod
-    def on_stop(self) -> None:
-        pass
-
-    @abstractmethod
-    def run(self) -> None:
-        pass
-
-    @abstractmethod
-    def generate(self) -> None:
+    def generate(self, time: float) -> None:
         pass
 
 
 class PassengerGenerator(AbstractPassengerGenerator):
 
-    def __init__(self, map: Map, frequency=1):
-        super(PassengerGenerator, self).__init__()
+    def __init__(self, map: Map):
+        super().__init__()
         self.map = map
-        self.frequency = frequency
-        self.hour = 0
 
-    def on_start(self) -> None:
-        self._thread_loop = Thread(target=self.run)
-        self._thread_loop.start()
-
-    def on_stop(self) -> None:
-        self._thread_loop.join()
-
-    def run(self) -> None:
-        while True:
-            time.sleep(1)  # TODO: add parameter
-            self.generate()
-            self.hour = self.hour + self.frequency % 24
-
-    def generate(self) -> None:
+    def generate(self, time: float) -> None:
         for _, stop in self.map.stops.items():
-            self.generate_for_one_stop(stop)
+            self.generate_for_one_stop(stop, time)
 
-    def generate_for_one_stop(self, stop: ActorProxy) -> None:
+    def generate_for_one_stop(self, stop: ActorProxy, time: float) -> None:
         possible_destinations = list(nx.descendants(self.map.topology, stop.id.get()))
         if not possible_destinations:
             return
-        number_of_passengers = stop.load_distribution.get().get_number_of_passengers(self.hour)
+        number_of_passengers = stop.load_distribution.get().get_number_of_passengers(time)
 
         passengers = []
         for _ in range(number_of_passengers):
